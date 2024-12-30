af743a01698992d9f807ca9261c754a3af0fdf62
Ahmed-Crystallite3 <ahmed.r@crystallitedigital.com>
2024-12-31 04:08:45 +0500

    fixes

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
         remotePatterns: [
             {
                 protocol: 'https',
-                hostname: 'inhouse.cryscampus.com',
+                hostname: 'inhouse.pulse-force.com',
                 port: '',
                 pathname: '/**',
             }
