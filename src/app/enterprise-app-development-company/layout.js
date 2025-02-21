ee18d2046811e20fc64120362d4f21171334e736
Ahmed-Crystallite3 <ahmed.r@crystallitedigital.com>
2025-02-21 21:36:01 +0500

    new page index

--- src/app/enterprise-app-development-company/layout.js
+++ src/app/enterprise-app-development-company/layout.js
@@ -2,28 +2,30 @@
 export const metadata = {
   //===== Meta Tags =====
   title: "Best Enterprise App Development Company in USA - Bitswits",
-  description: "Looking for an Enterprise App Development Company? We deliver scalable, secure apps that drive business growth. Get a free consultation today!",
+  description:
+    "Looking for an Enterprise App Development Company? We deliver scalable, secure apps that drive business growth. Get a free consultation today!",
   //===== OG Tags =====
   openGraph: {
-    title: 'Best Enterprise App Development Company in USA - Bitswits',
-    description: "Looking for an Enterprise App Development Company? We deliver scalable, secure apps that drive business growth. Get a free consultation today!",
-    url: '/enterprise-app-development-company',
-    siteName: 'BitsWits',
-    locale: 'en_US',
-    type: 'website',
+    title: "Best Enterprise App Development Company in USA - Bitswits",
+    description:
+      "Looking for an Enterprise App Development Company? We deliver scalable, secure apps that drive business growth. Get a free consultation today!",
+    url: "/enterprise-app-development-company",
+    siteName: "BitsWits",
+    locale: "en_US",
+    type: "website",
     images: "/public/images/icons/footerlogo.png",
   },
   //===== Canonical =====
-  alternates: { canonical: '/enterprise-app-development-company' },
+  alternates: { canonical: "/enterprise-app-development-company" },
   //===== GEO Tags =====
   other: {},
   //===== No-Index =====
   robots: {
-    index: false,
-    follow: false,
+    index: true,
+    follow: true,
   },
 }
 
 export default function RootLayout({ children }) {
-  return (children);
+  return children
 }
